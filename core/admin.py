64740c078d822067032be1ff581fe387c31ff0ad
Behailu Sileshi <behailusileshi7@gmail.com>
2025-04-10 23:59:19 +0300

    Create custom User model with role support and register it in UserAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'role')
+    list_filter = ('role',)
+    ordering = ('email',)
+    search_fields = ('email', 'first_name', 'last_name')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('role',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'role', 'first_name', 'last_name'),}
+        ),
+    )
+
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
+from enum import unique
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    ADMIN = 0
+    VENDOR = 1
+    CUSTOMER = 2
+    ROLE_CHOICES = (
+        (ADMIN, 'Admin'),
+        (VENDOR, 'Vendor'),
+        (CUSTOMER, 'Customer'),
+    )
+    username = None
+    email = models.EmailField(unique=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, default=CUSTOMER)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return self.email
